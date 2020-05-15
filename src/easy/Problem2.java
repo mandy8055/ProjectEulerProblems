b79f3ad0e94073e6bb22c0578c73ae718661f476
Mandy8055 <msankrit8055@gmail.com>
2020-05-15 19:34:13 +0530

    Initial Commit

---
+++ src/easy/Problem2.java
@@ -0,0 +1,9 @@
+package easy;
+
+import utility.Fibonacci;
+
+public class Problem2 {
+    public static void main(String[] args){
+        System.out.println(Fibonacci.fibonacciIterative(4000000L));
+    }
+}
